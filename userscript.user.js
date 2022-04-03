5854c5f9946f9ca1fec8640ec27e6246e7c024a2
dfons25 <davidmgafonso@gmail.com>
2022-04-04 00:12:28 +0100

    new2

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/dfons25/draconBot/main/canvas_pt2.png";
+            i.src = "https://res.cloudinary.com/adotaqui/image/upload/v1649027499/hdm2.png";
             i.style = "position: absolute;left: 0;top: 0px;image-rendering: pixelated;width: 2000px;height: 2000px";
             i.id = "mcss-overlay"
             i.setAttribute("vis",1)
